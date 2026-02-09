5039a0edcc63db17c70108fa7348498150747e8c
Rohit <rohit123@gmail.com>
2026-02-09 18:23:47 +0530

    Hwllo

--- demo/src/main/java/com/project/demo/config/CrosConfig.java
+++ demo/src/main/java/com/project/demo/config/CrosConfig.java
@@ -6,27 +6,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class CrosConfig {
     @Bean
-    public WebMvcConfigurer webMvcConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                        "http://localhost:5173",
-                        "https://rohitgusai.github.io/Smart-Workflow-Audit-Management-System/"
-                )
-                        .allowedMethods("GET","POST","PUT","DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+    public CorsConfigurationSource corsConfigurationSource() {
 
-            }
-        };
+        CorsConfiguration config = new CorsConfiguration();
 
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://rohitgusai.github.io"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
+
 
     @Bean
     public RestTemplate restTemplate() {
